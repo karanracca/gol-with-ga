bb7149c78ea2719e8637e15982d31c4e56c41e28
Shubhankar Dandekar <shubhankard34@gmail.com>
2019-12-07 21:13:11 -0500

    Revert "GA, Life, Library and Runner changes"

--- geneticgameoflife/src/main/java/runner/Runner.java
+++ geneticgameoflife/src/main/java/runner/Runner.java
@@ -1,9 +1,7 @@
 package runner;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import ga.DNA;
 import ga.GA;
-import library.Library;
 import life.Life;
 import ui.UI;
 import ga.Population;
@@ -17,15 +15,14 @@ import java.io.IOException;
 
 public class Runner {
 
-    private static final int FRAME_SIZE = Library.FRAME_SIZE;
-    private static final int GRID_SIZE = Library.GRID_SIZE;
-    private static final int POPULATION_SIZE = Library.POPULATION_SIZE;
+    private static final int FRAME_SIZE = 500;
+    private static final int GRID_SIZE = 100;
+    private static final int POPULATION_SIZE = 100;
 
-    public static void checkGrowth() throws IOException{
+    public static void checkGrowth() {
         Life.MAX_ALIVE = 90;
         Life.MIN_ALIVE = 0;
-        Life.SPEED = 25;
-        Life.CREATE_CSV = true;
+        Life.SPEED = 100;
         BufferedImage img = null;
         BufferedImage rgbImage = new BufferedImage(GRID_SIZE, GRID_SIZE, BufferedImage.TYPE_INT_RGB);
         try {
@@ -36,8 +33,8 @@ public class Runner {
         int[] pixels = ((DataBufferInt) rgbImage.getRaster().getDataBuffer()).getData();
         System.out.println(pixels.length);
         boolean[] pattern = new boolean[pixels.length];
-        for (int i = 0; i < pixels.length; i++) {
-            if (pixels[i] == 0xffffff) {
+        for(int i = 0; i < pixels.length; i++) {
+            if(pixels[i] == 0xffffff) {
                 pattern[i] = true;
             } else {
                 pattern[i] = false;
@@ -55,21 +52,16 @@ public class Runner {
     }
 
 
-    public static void main(String[] args) throws IOException{
-        boolean runBestInitial = Boolean.valueOf(args[0]);
-        if (runBestInitial) {
-            checkGrowth();
-        } else {
-            UI ui = new UI(Runner.FRAME_SIZE, Runner.GRID_SIZE);
-            Population initialPopulation = new Population(POPULATION_SIZE, GRID_SIZE);
+    public static void main(String[] args) {
 
-            Life.MIN_ALIVE = 1;
-            Life.MAX_ALIVE = 95;
-            Life.SPEED = 0;
-            Life.CREATE_CSV = false;
+        UI ui = new UI(Runner.FRAME_SIZE, Runner.GRID_SIZE);
+        Population initialPopulation =  new Population(POPULATION_SIZE, GRID_SIZE);
 
-            GA ga = new GA(ui, initialPopulation);
-        }
+        Life.MIN_ALIVE = 5;
+        Life.MAX_ALIVE = 95;
+        Life.SPEED = 0;
+
+        GA ga = new GA(ui, initialPopulation);
 
 
     }
